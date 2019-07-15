b64edc9caf73e3454c083d5cda381af5604e1b67
Akshayus27 <52912821+Akshayus27@users.noreply.github.com>
2019-07-15 16:17:26 +0530

    Update third.py

--- third.py
+++ third.py
@@ -2,7 +2,7 @@ i = str(input())
 ii = i.upper()
 k = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 if(ii in k):
-    if(ii == ("A"or"E"or"I"or"O"or"U")):
+    if(ii=="A" or ii=="E" or ii=="I" or ii=="O" or ii=="U"):
        print("Vowel")
     else:
        print("Consonant")
