ba61ba896dc5df4e08ecf28170537b939d22821c
Akshayus27 <52912821+Akshayus27@users.noreply.github.com>
2019-07-19 11:32:52 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -4,12 +4,11 @@ for i in range(1, n+1):
   i = str(input())
   n1.append(sorted(i))
 count = 0
-g = 0
+g = 1
 for g in range(g, len(n1)):
   if g < len(n1)-1:
-    if n1[g]!= n1[g+1]:
+    if n1[0]!= n1[g]:
       count += 1
     else:
       continue
-  g -= 1
 print(abs(len(n1)-count))
